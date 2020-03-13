f91038bfd070416202b813d1d2b935b606193c0f
Matthew Lui <matthew_kin_lui@manulife.com>
2020-03-13 17:40:03 +0800

    Initial Commit

---
+++ Clean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean
+//
+//  Created by Mr. Matthew Lui （呂健） on 13/3/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
